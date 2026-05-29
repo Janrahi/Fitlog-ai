0324e4b732a280ba9a3b2c2017adc77978fb3730
Janrahi <janrahi@gmail.com>
2026-05-29 06:20:44 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+var CACHE = 'fitlog-v1';
+var FILES = ['./index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(caches.match(e.request).then(function(cached){
+    return cached || fetch(e.request).catch(function(){ return caches.match('./index.html'); });
+  }));
+});
